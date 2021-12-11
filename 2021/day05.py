b2957f3e8e7b21f22eac24ec16030d5db001e136
Austin LaFever <57731062+Austinll0@users.noreply.github.com>
2021-12-11 04:17:53 +0000

    day5 p2

--- 2021/day05.py
+++ 2021/day05.py
@@ -1,50 +1,8 @@
-class hashMap:
-  def __init__(self, mapSize):
-    self.Map = [];
-    self.mapSize = mapSize
-    for i in range(mapSize):
-      self.Map.append([])
-
-  def makeKey(self,point):
-    #print(point)
-    
-    key = point[0] * point[1] % self.mapSize
-    #print(key)
-    return key
-  def append(self, point):
-    self.Map[self.makeKey(point)].append(point)
-  
-  def contains(self,point):
-    for p in self.Map[self.makeKey(point)]:
-      if p[0] == point[0] and p[1] == point[1]:
-        #print(p)
-        #print(point)
-        return True
-      print(p)
-      print(point)
-      print(" ")
-
-    return False
-  
-  def pop(self):
-    for a in self.Map:
-      if a:
-        return a.pop(0)
-    return []
-  def isEmpty(self):
-    for a in self.Map:
-      if a:
-        return False
-    return True
-  def print(self):
-    for a in self.Map:
-      for b in a:
-        print(b)
-
+import numpy as np
 
 class Line:
   def __init__(self,p1,p2):
-      mapSize = 50
+      mapSize = 100
       p1 = [int(p1[0]), int(p1[1])]
       p2 = [int(p2[0]), int(p2[1])]
 
@@ -67,25 +25,42 @@ class Line:
              
 
 Lines = []
-intersections = []
-
+intersections = 0
+map = np.zeros([1000, 1000])
 with open("2021/day05in.txt") as f:
   for input in f.readlines():
     points = input.split(" -> ")
-    Lines.append(Line(points[0].split(","), points[1].split(",")))
+    p1 = points[0].split(",")
+    p1 = [int(p1[0]),int(p1[1])]
+    p2 = points[1].split(",")
+    p2 = [int(p2[0]),int(p2[1])]
+    # vertical case
+    if(p1[0] == p2[0]):
+      start = min([p1[1], p2[1]])
+      end = max(p1[1],p2[1])
+      for i in range(start,end+1):
+        map[p1[0]][i] = map[p1[0]][i] + 1
+    # horizontal case
+    elif(p1[1] == p2[1]):
+      start = min([p1[0], p2[0]])
+      end = max(p1[0],p2[0])
+      for i in range(start,end+1):
+        map[i][p1[1]] = map[i][p1[1]] + 1
+    else:
+      #make p1 the more left point to simplify
+      if(p1[1] > p2[1]):
+        temp = p1;
+        p1 = p2;
+        p2 = temp
+      if(p1[0] < p2[0]):
+        for i in range(1 + p2[0]-p1[0]):
+          map[p1[0] + i][p1[1] + i] =map[p1[0] + i][p1[1] + i] + 1
+      else: 
+        for i in range(1 + p1[0]-p2[0]):
+          map[p1[0] - i][p1[1] + i] =map[p1[0] - i][p1[1] + i] + 1
 
-while len(Lines):
-  #print(len(Lines))
-  l1 = Lines.pop(0)
-  l1.points.print()
-  for l2 in Lines:
-    while not l1.points.isEmpty():
-      p1 = l1.points.pop()
-      #print(p1)
-      #print("p1: " + str(p1))
-      if l2.points.contains(p1):
-        intersections.append(p1)
-print(len(intersections))
-          
-
-
+for i in range(1000):
+  for j in range(1000):
+    if(map[i][j] > 1):
+      intersections = intersections + 1
+print(intersections)
