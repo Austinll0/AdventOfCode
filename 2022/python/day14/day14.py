1cb730596eac4fc5f1440d94d5b090078091fb29
Austinll0 <austinl@localhost.localdomain>
2022-12-14 20:22:57 -0500

    day14

--- 2022/python/day14/day14.py
+++ 2022/python/day14/day14.py
@@ -1,3 +1,72 @@
+import time;
+
+class hashMap:
+    def __init__(self,size):
+        self.size = size;
+        self.map = [];
+        for i in range(size):
+            self.map.append([]);
+    def __contains__(self,item):
+        return item in self.map[self.createKey(item)]
+    def createKey(self,item):
+        return (item[0] * item[1]) % self.size;
+    def append(self,item):
+        self.map[self.createKey(item)].append(item);
+    def extend(self,items):
+        for i in items:
+            self.append(i);
+    def getMaxY(self):
+        max = 0;
+        for a in self.map:
+            for i in a:
+                if i[1] > max:
+                    max = i[1];
+        return max;
+def pour(points, pos,yMax):
+    while True:
+        if pos[1] > yMax:
+            return;
+        new = [pos[0],pos[1]+1];
+        if tuple(new) in points:
+            new = [pos[0]-1,pos[1]+1];
+            if tuple(new) in points:
+                new[0] = new[0]+2;
+                if tuple(new) in points:
+                    return pos;
+                else:
+                    pos[0] = pos[0] + 1;
+                    continue;
+            else:
+                pos[0] = pos[0] -1;
+                continue;
+        else:
+            pos[1] += 1;
+
+def pour2(points, pos,yMax):
+    yMax += 2;
+    while True:
+        if pos[1] == yMax - 1:
+            return [pos[0],pos[1]];
+        new = [pos[0],pos[1]+1];
+        if tuple(new) in points:
+            new = [pos[0]-1,pos[1]+1];
+            if tuple(new) in points:
+                new[0] = new[0]+2;
+                if tuple(new) in points:
+                    if pos[0] == 500 and pos[1] == 0:
+                        return;
+                    return pos;
+                else:
+                    pos[0] = pos[0] + 1;
+                    pos[1] = pos[1] +1;
+                    continue;
+            else:
+                pos[0] = pos[0] -1;
+                pos[1] = pos[1] +1;
+                continue;
+        else:
+            pos[1] += 1;
+
 def points(line):
     points = [i.strip().split(",") for i in line.split("->")]
     out = [];
@@ -14,5 +83,27 @@ def points(line):
     out = [tuple(i) for i in out]
     return out;
 
-for line in open("day14ex.txt"):
-    print(points(line));
+start = time.time();
+
+plist = hashMap(5000);
+for line in open("day14in.txt"):
+    plist.extend(points(line));
+yMax = plist.getMaxY();
+P1 = 0;
+new = pour(plist,[500,0],yMax);
+while new:
+    plist.append(tuple(new));
+    new = pour(plist,[500,0],yMax);
+    P1 += 1;
+print("Part 1: " ,P1);
+
+P1 = P1 + 1;
+new = pour2(plist,[500,0],yMax);
+while new:
+    plist.append(tuple(new));
+    new = pour2(plist,[500,0],yMax);
+    P1 += 1;
+
+    
+print("Part 2: ",P1);
+print("elapsed time: ",time.time() - start);
